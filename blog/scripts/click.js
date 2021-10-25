ca5d001c90d22e8ae8f4eb3a99d615df4baadd93
estella00911 <estella00911@gmail.com>
2021-10-26 07:46:36 +0800

    delete blog files

--- blog/scripts/click.js
+++
@@ -1,8 +0,0 @@
-// let urlBase = 'http://estella00911.tw/w11_blog';
-const urlBase = 'https://frozen-springs-86838.herokuapp.com'
-const articlePost = document.querySelectorAll('.article__post')
-for (let i = 0; i < articlePost.length; i++) {
-  articlePost[i].addEventListener('click', (e) => {
-    location.href = `${urlBase}/article/${e.currentTarget.id}`
-  })
-}
