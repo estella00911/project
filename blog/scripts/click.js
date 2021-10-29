877115c3e8f6d0bf733565959fd19c8c66862f41
estella00911 <estella00911@gmail.com>
2021-10-29 16:19:34 +0800

    updage base url for single article page

--- blog/scripts/click.js
+++ blog/scripts/click.js
@@ -1,5 +1,4 @@
-// let urlBase = 'http://estella00911.tw/w11_blog';
-const urlBase = 'https://frozen-springs-86838.herokuapp.com'
+const urlBase = 'https://gentle-depths-67267.herokuapp.com'
 const articlePost = document.querySelectorAll('.article__post')
 for (let i = 0; i < articlePost.length; i++) {
   articlePost[i].addEventListener('click', (e) => {
